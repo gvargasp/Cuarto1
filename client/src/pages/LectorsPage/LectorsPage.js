9a40d4efade1da3aff57a4ef63c03b9e645f1d53
Gina Vargas <gvargasp@hotmail.com>
2022-10-21 11:39:27 -0300

    Incorporar Cors

--- client/src/pages/LectorsPage/LectorsPage.js
+++ client/src/pages/LectorsPage/LectorsPage.js
@@ -1,6 +1,15 @@
 import { Link } from 'react-router-dom'
 
 const LectorsPage = () => {
+
+  const loadLectors = () => {
+    fetch('http://localhost:5005/api/lectors')
+      .then(res => res.json())
+      .then(allLectors => console.log(allLectors))
+  }
+  
+  loadLectors()
+  
   return (
     <main>
       <h1>Esta es la LISTA</h1>
--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const app = express()
 
+//DB conecction
 const mongoose = require('mongoose')
 mongoose
   .connect('mongodb://localhost/Biblioteca')
   .then(() => console.log('conectado a BBDD'))
 
+  // Models
 const Lector = require('./Models/Lector.model')
 
+// Cors
+const cors = require('cors')
+app.use(cors())
+
+// Routers
 app.get('/api/lectors', (req, res) => 
   {
     Lector
@@ -15,7 +22,6 @@ app.get('/api/lectors', (req, res) =>
       .then(allLectors => res.json(allLectors))
 
 })
-
 
 app.get('/api/details/:lector_id', (req, res) => {
 
