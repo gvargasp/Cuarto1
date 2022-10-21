032482393a9ba5cfbae0bbd6730180ecf09728c5
Gina Vargas <gvargasp@hotmail.com>
2022-10-20 23:53:08 -0300

    Conexión a la BBDD

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const app = express()
 
+const mongoose = require('mongoose')
+mongoose
+  .connect('mongodb://localhost/Biblioteca')
+  .then(() => console.log('conectado a BBDD'))
+
 app.listen(5005, () => console.log('SERVER ARRIBA'))
